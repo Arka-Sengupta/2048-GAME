5107715b7dc932a757b5d65c850ee39f4607863a
Arka-Sengupta <arka.sengupta.06@gmail.com>
2024-09-07 12:43:51 +0530

    Update logic.py

--- logic.py
+++ logic.py
@@ -59,28 +59,3 @@ def check_lose(board):
             if board[i][j] == board[i][j+1] or board[j][i] == board[j+1][i]:
                 return False
     return True
-# def main():
-#     board = initialise_board()
-#     while True:
-#         display_board(board)
-#         if check_win(board) is True:
-#             print("You Won the game")
-#             break
-#         if check_lose(board) is True:
-#             print("You Lose")
-#             break
-#         user_move = input("Press W/A/S/D for swiping up/left/down/right: ").lower()
-#         if user_move =="w":
-#             board = swipe_up(board)
-#         elif user_move == "a":
-#             board = swipe_left(board)
-#         elif user_move == "s":
-#             board = swipe_down(board)
-#         elif user_move == "d":
-#             board = swipe_right(board)
-#         else:
-#             print("Invalid move")
-#             continue
-#         add_new_tile(board)
-# if __name__ == "__main__":
-#     main()
